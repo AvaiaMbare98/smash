5d39e382f2068e1850c42ad02cc3b0aa34c1b106
Giorgio <giograsso98@gmail.com>
2026-03-27 13:56:42 +0100

    Add read_line() in main.c and started writing smash_loop() in main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+char* read_line();
+void smash_loop();
 
 int main(int argc, char **argv){
-
+    //smash_loop();
     return 0;
+}
+
+void smash_loop(){
+    char* line;
+    char** args;
+
+    /*do {
+        printf("> ");
+        //Strumentopoli misteriosi per dopo
+        //line = read_line(); 
+        //args = arg_parser(line);
+
+        free(line);
+        free(args);
+    }while(1);*/
+
+}
+
+char* read_line(){
+    char* line = NULL;
+    size_t buffer_size = 0;
+
+    if(getline(&line, &buffer_size, stdin) == -1){
+        if(feof(stdin)){
+            exit(EXIT_SUCCESS);
+        }
+        else{
+            exit(EXIT_FAILURE);
+        }
+    }
+    return line;
 }
