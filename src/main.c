1c13948d461e131e16412c78496e9c0cb201eb55
Giorgio Grasso <giograsso98@gmail.com>
2026-04-11 12:15:10 +0200

    Include parser.h

--- src/main.c
+++ src/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+#include <unistd.h> // library for POSIX syscalls
+#include "parser.h"
 
 char* read_line();
 void smash_loop();
 
 int main(int argc, char **argv){
-    //smash_loop();
+    smash_loop();
     return 0;
 }
 
@@ -15,20 +16,19 @@ void smash_loop(){
     char* line;
     char** args;
 
-    /*do {
+    do {
         printf("> ");
-        //Strumentopoli misteriosi per dopo
-        //line = read_line(); 
-        //args = arg_parser(line);
+        line = read_line(); 
+        args = arg_parser(line);
 
         free(line);
         free(args);
-    }while(1);*/
+    }while(1);
 
 }
 
 char* read_line(){
-    char* line = NULL;
+    char* line = NULL; 
     size_t buffer_size = 0;
 
     if(getline(&line, &buffer_size, stdin) == -1){
