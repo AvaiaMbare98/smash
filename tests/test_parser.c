aaa913d49f5dd02e7e383ed7c175841f343c5bdd
Giorgio Grasso <giograsso98@gmail.com>
2026-04-11 12:40:02 +0200

    Add first test: arg_number

--- tests/test_parser.c
+++ tests/test_parser.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <criterion/criterion.h>
+#include <criterion/new/assert.h>
 #include <string.h>
+#include "parser.h"
 
-Test(test_parser, test){
-    cr_assert(strlen("") == 0);
+Test(test_parser, arg_number){
+    char* input = "ls -l -a";
+
+    cr_assert(eq(int, arg_parser(input).args_count, 3));
 }
