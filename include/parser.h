8531df781a033fd67ebb7dba72cd9f5fa9c95930
Giorgio Grasso <giograsso98@gmail.com>
2026-04-11 12:41:18 +0200

    Add tokenized_line struct and arg_parser declaration

--- include/parser.h
+++ include/parser.h
@@ -1,10 +1,12 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-typedef struct command_line{
+typedef struct tokenized_line{
     char** args; // the arguments of the command
     int args_count; // number of arguments
     int memory; // how much memory have we allocated?
-} command_line;
+} tokenized_line;
+
+tokenized_line arg_parser(char* line);
 
 #endif 
