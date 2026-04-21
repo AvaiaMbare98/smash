c885e6dc1ce41b78fd441b5b3a99e5b457af9d4f
Giorgio Grasso <giograsso98@gmail.com>
2026-04-21 14:49:14 +0200

    Add tests: empty_string, empty_spaces. memory_test

--- tests/test_parser.c
+++ tests/test_parser.c
@@ -6,6 +6,36 @@
 
 Test(test_parser, arg_number){
     char input[] = "ls -l -a";
+    tokenized_line tok_line = arg_parser(input);
 
-    cr_assert(eq(int, arg_parser(input).args_count, 3));
+    cr_assert(eq(int, tok_line.num, 3));
+
+    strcpy(input, "cd /..");
+    tok_line = arg_parser(input);
+
+    cr_assert(eq(int, tok_line.num, 2));
+}
+
+Test(test_parser, empty_string){
+    char input[] = "";
+    tokenized_line tok_line = arg_parser(input);
+
+    cr_assert(eq(int, tok_line.num, 0));
+}
+
+Test(test_parser, empty_spaces){
+    char input[] = "      ls        -l      a      ";
+    tokenized_line tok_line = arg_parser(input);
+
+    cr_assert(eq(int, tok_line.num, 3));
+    cr_assert(eq(str, tok_line.args[0], "ls"));
+    cr_assert(eq(str, tok_line.args[1], "-l"));
+    cr_assert(eq(str, tok_line.args[2], "a"));
+}
+
+Test(test_parser, memory_test){
+    char input[] = "Di a da in con su per tra fra sbo";
+    tokenized_line tok_line = arg_parser(input);
+
+    cr_assert(eq(int, tok_line.num, 10));
 }
