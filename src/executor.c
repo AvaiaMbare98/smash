5815750ca924daa2f0a5b6000e04d70136c63aba
Giorgio Grasso <giograsso98@gmail.com>
2026-05-07 20:32:18 +0200

    Add empty string condition and includes

--- src/executor.c
+++ src/executor.c
@@ -1,10 +1,16 @@
 #include "executor.h"
+#include "smash_builtins.h"
+#include "smash_launch.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 int process_executor(char** args){
+
+    if(args[0] == NULL)
+        return 1; // empty string, return 1 to keep looping!
+
 
     
 }
