95d57e451718331bcca3d514d20459c11a3d943b
Giorgio Grasso <giograsso98@gmail.com>
2026-05-07 20:31:41 +0200

    Add boilerplate

---
+++ src/smash_builtins.c
@@ -0,0 +1,4 @@
+#include "smash_builtins.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
---
+++ src/smash_launch.c
@@ -0,0 +1,4 @@
+#include "smash_launch.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
