8b7d590a2e6d4a30bcd364cda4672f0df1b32732
Giorgio Grasso <giograsso98@gmail.com>
2026-05-08 16:22:27 +0200

    Modify declaration of (*builtin_func[]) and builtin_str; add declaration of smash_launch

--- include/smash_builtins.h
+++ include/smash_builtins.h
@@ -8,16 +8,8 @@ int builtin_num();
 
 // This is pretty new for me: an array of pointers to functions!
 // I heard about it some time ago but never implemented it. :)
-int (*builtin_func[]) (char**) = {
-    &smash_cd,
-    &smash_exit,
-    &smash_help
-};
+extern int (*builtin_func[]) (char**);
 
-char* builtin_str[] = {
-    "cd",
-    "exit",
-    "help"
-};
+extern char* builtin_str[];
 
 #endif 
--- include/smash_launch.h
+++ include/smash_launch.h
@@ -1,6 +1,6 @@
 #ifndef SMASH_LAUNCH_H
 #define SMASH_LAUNCH_H
 
-
+int smash_launch(char** args);
 
 #endif 
