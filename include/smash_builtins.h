b760d00c9a4f5035d52acf75ceb0be81e0836d7d
Giorgio Grasso <giograsso98@gmail.com>
2026-05-08 16:01:14 +0200

    Add builtin_num() header

--- include/smash_builtins.h
+++ include/smash_builtins.h
@@ -4,9 +4,10 @@
 int smash_cd(char** args);
 int smash_exit(char** args);
 int smash_help(char** args);
+int builtin_num();
 
 // This is pretty new for me: an array of pointers to functions!
-// I heard about it some time ago but never implented it. :)
+// I heard about it some time ago but never implemented it. :)
 int (*builtin_func[]) (char**) = {
     &smash_cd,
     &smash_exit,
